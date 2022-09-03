2b6693fe48c4a78fa7f2c4500f5fd7b7cf848f87
ejioguvictor <ejioguvictor6054@gmail.com>
2022-09-03 14:35:33 +0100

    started examine task, installed tailwind and started the login page

---
+++ components/LoginForm/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div>
+      <h1 className="">Welcome back! 👋</h1>
+      <h2 className=""> Let build something great</h2>
+      <form action=""></form>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'primary-color': '#562E69',
+      'examine-gray': '#6C757D',
+      'trial-bg': 'rgba(37, 161, 175, 0.1)',
+      'trial-text': '#1B818C',
+      
+    },
+  },
+  plugins: [],
+}
